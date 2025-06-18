70825335a0e295d87ea71000e3919ad130594e95
Leonardo Lüders <luderser@hotmail.com>
2025-06-18 09:10:55 -0300

    Finished class 231 — Created and received the request body from HTTP methods POST/PATCH/PUT.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  login() {
-    return 'Olá AuthService!';
+  login(loginDto: LoginDto) {
+    console.log(loginDto.email);
+    console.log(loginDto.password);
+    return loginDto;
   }
 }
