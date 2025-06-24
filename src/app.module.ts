ec0ac59f53f7060e3a64637aad6f733f2a0164bc
Leonardo Lüders <luderser@hotmail.com>
2025-06-24 10:25:05 -0300

    Finished class 235 — Overview of the environment variables, .env file and ConfigModule from nestjs/config package.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CustomParseInPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get(':id')
   findOne(@Param('id', CustomParseInPipe) id: number) {
-    console.log(id, typeof id);
+    console.log(this.configService.getOrThrow('TESTE'));
     return `Olá UserController!, id:${id}`;
   }
 }
