f954f595746562a598c46fd11b5043d863944ba4
Leonardo Lüders <luderser@hotmail.com>
2025-06-24 11:18:33 -0300

    Finished class 236 — Created a database using TypeOrmModule (TypeORM) and the dot-env variables.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { CustomParseInPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly configService: ConfigService) {}
-
   @Get(':id')
   findOne(@Param('id', CustomParseInPipe) id: number) {
-    console.log(this.configService.getOrThrow('TESTE'));
     return `Olá UserController!, id:${id}`;
   }
 }
