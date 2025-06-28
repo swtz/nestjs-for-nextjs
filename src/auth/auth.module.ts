f5b7fad7056e9918b2bb49e449a68cfa1d1fcf9c
Leonardo Lüders <luderser@hotmail.com>
2025-06-28 11:21:34 -0300

    Class 239 — Injected UserService and HashingService into AuthService by importing their modules into AuthModule.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
+  imports: [UserModule, CommonModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { UserService } from 'src/user/user.service';
+import { HashingService } from 'src/common/hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly hashingService: HashingService,
+  ) {}
+
   login(loginDto: LoginDto) {
     return loginDto;
   }
