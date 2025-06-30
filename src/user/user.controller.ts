32d9972805001facd53e65a41c4c98eab26f0eed
Leonardo Lüders <luderser@hotmail.com>
2025-06-30 12:34:57 -0300

    Finished class 242 — Secured the dynamic user route using JwtAuthGuard (a custom AuthGuard).

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,29 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { CustomParseInPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id', CustomParseInPipe) id: number) {
+  findOne(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', CustomParseInPipe) id: number,
+  ) {
+    console.log(req.user.email);
     return `Olá UserController!, id:${id}`;
   }
 
