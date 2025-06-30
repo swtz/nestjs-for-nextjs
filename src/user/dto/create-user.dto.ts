668171324126f81dde21b2efa8da0e990724f592
Leonardo Lüders <luderser@hotmail.com>
2025-06-30 16:06:08 -0300

    Class 244 — Created the UserResponseDto class to structure controller response object.

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,7 +2,7 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString({ message: 'Formato inválido' })
-  @IsNotEmpty({ message: 'O nome não pode estar vazia' })
+  @IsNotEmpty({ message: 'O nome não pode estar vazio' })
   name: string;
 
   @IsEmail({}, { message: 'E-mail inválido' })
