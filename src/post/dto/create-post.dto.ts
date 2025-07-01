6c61cb4f970f50b6ff3b753bd033bac90685c91f
Leonardo Lüders <luderser@hotmail.com>
2025-07-01 10:35:18 -0300

    Class 248 — Created the UpdatePostDto class.

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -7,15 +7,15 @@ import {
 } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString({ message: 'Título precisa ser um texto' })
+  @IsString({ message: 'Título precisa ser uma string' })
   @Length(10, 150, { message: 'Título precisa ter entre 10 e 150 caracteres.' })
   title: string;
 
-  @IsString({ message: 'Excerto precisa ser um texto' })
+  @IsString({ message: 'Excerto precisa ser uma string' })
   @Length(10, 200, { message: 'Título precisa ter entre 10 e 200 caracteres.' })
   excerpt: string;
 
-  @IsString({ message: 'Conteúdo precisa ser um texto' })
+  @IsString({ message: 'Conteúdo precisa ser uma string' })
   @IsNotEmpty({ message: 'Conteúdo não pode estar vazio' })
   content: string;
 
--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, PickType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(
+  PickType(CreatePostDto, ['title', 'excerpt', 'content', 'coverImageUrl']),
+) {
+  @IsOptional() // Vai depender da lógica que criarmos no service ou no Next.js
+  @IsBoolean({ message: 'Campo de publicar post precisa ser boolean' })
+  published?: boolean;
+}
