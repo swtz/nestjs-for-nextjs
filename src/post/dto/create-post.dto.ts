cc9d9593d026b540c1828eb5d4d9407b9238a5ec
Leonardo Lüders <luderser@hotmail.com>
2025-07-01 10:28:22 -0300

    Class 248 — Created the CreatePostDto class.

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,25 @@
-export class CreatePostDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString({ message: 'Título precisa ser um texto' })
+  @Length(10, 150, { message: 'Título precisa ter entre 10 e 150 caracteres.' })
+  title: string;
+
+  @IsString({ message: 'Excerto precisa ser um texto' })
+  @Length(10, 200, { message: 'Título precisa ter entre 10 e 200 caracteres.' })
+  excerpt: string;
+
+  @IsString({ message: 'Conteúdo precisa ser um texto' })
+  @IsNotEmpty({ message: 'Conteúdo não pode estar vazio' })
+  content: string;
+
+  @IsOptional() // Vai ser requerido pelo Next.js
+  @IsUrl({ require_tld: false }) // Top level domain proíbe localhost e IP
+  coverImageUrl?: string;
+}
