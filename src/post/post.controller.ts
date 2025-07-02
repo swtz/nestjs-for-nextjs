4353770fbb5d40c1732d63b9596d69be645f9bb3
Leonardo Lüders <luderser@hotmail.com>
2025-07-02 10:34:50 -0300

    Class 252 — Implemented findOneOwned() in the post controller.

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { PostService } from './post.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
@@ -16,6 +25,16 @@ export class PostController {
     @Body() postDto: CreatePostDto,
   ) {
     const post = await this.postService.create(postDto, req.user);
+    return new ResponsePostDto(post);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me/:id')
+  async findOneOwned(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    const post = await this.postService.findOneOwnedOrFail({ id }, req.user);
     return new ResponsePostDto(post);
   }
 }
