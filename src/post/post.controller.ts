fb0f47eb1d86e20476cfe29bfa943a0a577a9e22
Leonardo Lüders <luderser@hotmail.com>
2025-07-02 10:52:41 -0300

    Finished class 252 — Implemented findAllOwned() in the post controller and tested routes.

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -37,4 +37,11 @@ export class PostController {
     const post = await this.postService.findOneOwnedOrFail({ id }, req.user);
     return new ResponsePostDto(post);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async findAllOwned(@Req() req: AuthenticatedRequest) {
+    const posts = await this.postService.findAllOwned(req.user);
+    return posts.map(post => new ResponsePostDto(post));
+  }
 }
