f83cb7cf91b3269975c93fc676213f003aae2532
Leonardo Lüders <luderser@hotmail.com>
2025-07-02 08:54:27 -0300

    Class 251 — Handled an exception thrown by repository and replaced it with a custom one.

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { User } from 'src/user/entities/user.entity';
 import { Post } from './entities/post.entity';
+import { createSlugFromText } from 'src/common/utils/create-slug-from-text';
 
 @Injectable()
 export class PostService {
@@ -14,14 +15,19 @@ export class PostService {
 
   async create(postDto: CreatePostDto, author: User) {
     const post = this.postRepository.create({
-      slug: 'caiu-na-pegadinha-' + Math.random().toString(36).substring(2, 8), // slug temporário
+      // slug: 'caiu-na-pegadinha-' + Math.random().toString(36).substring(2, 8), // slug temporário
+      slug: createSlugFromText(postDto.title),
       title: postDto.title,
       excerpt: postDto.excerpt,
       content: postDto.content,
       author,
     });
 
-    const created = await this.postRepository.save(post);
+    const created = await this.postRepository
+      .save(post)
+      .catch((err: unknown) => {
+        throw new BadRequestException('Erro ao criar o post');
+      });
 
     return created;
   }
