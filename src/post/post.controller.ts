9021aa79b3801176c9281c2bd53cf54e90fe449c
Leonardo Lüders <luderser@hotmail.com>
2025-07-03 10:10:05 -0300

    Finished class 253 — Implemented update() in the post controller and tested routes.

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   Param,
   ParseUUIDPipe,
+  Patch,
   Post,
   Req,
   UseGuards,
@@ -13,6 +14,7 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 import { CreatePostDto } from './dto/create-post.dto';
 import { ResponsePostDto } from './dto/response-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('post')
 export class PostController {
@@ -43,5 +45,16 @@ export class PostController {
   async findAllOwned(@Req() req: AuthenticatedRequest) {
     const posts = await this.postService.findAllOwned(req.user);
     return posts.map(post => new ResponsePostDto(post));
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me/:id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Req() req: AuthenticatedRequest,
+    @Body() postDto: UpdatePostDto,
+  ) {
+    const post = await this.postService.update({ id }, postDto, req.user);
+    return new ResponsePostDto(post);
   }
 }
