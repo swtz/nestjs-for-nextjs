e0ce04db45d7287aca69533cc12a680510566566
Leonardo Lüders <luderser@hotmail.com>
2025-07-03 10:34:08 -0300

    Class 254 — Implemented findAll() in the post service.

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -76,6 +76,18 @@ export class PostService {
     return posts;
   }
 
+  async findAll(postData: Partial<Post>) {
+    const posts = await this.postRepository.find({
+      where: postData,
+      order: {
+        createdAt: 'DESC',
+      },
+      relations: ['author'],
+    });
+
+    return posts;
+  }
+
   async create(postDto: CreatePostDto, author: User) {
     const post = this.postRepository.create({
       slug: createSlugFromText(postDto.title),
