f3fa5d038f496f45d14acdcd133400358582efce
Leonardo Lüders <luderser@hotmail.com>
2025-07-04 09:51:58 -0300

    Class 256 — Implemented uploadFile() in the upload controller — #IMPORTANT

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { UploadService } from './upload.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter, limits, storage } from './upload.config';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('file', { storage, limits, fileFilter }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.uploadService.handleUpload(file);
+  }
+}
