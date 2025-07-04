8f3d24730c61890d1c0b429fc66a569b8074addf
Leonardo Lüders <luderser@hotmail.com>
2025-07-04 10:24:39 -0300

    Class 256 — Added ServerStaticModule to the upload module.

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, '..', '..', 'uploads'),
+      serveRoot: '/uploads', // define a URL base
+      serveStaticOptions: {
+        fallthrough: false, // não continua caso não encontre o arquivo
+        index: false, // impede tentativa de servir "index.html"
+      },
+    }),
+  ],
   controllers: [UploadController],
   providers: [UploadService],
 })
